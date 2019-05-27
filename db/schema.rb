4caa8dfa3538dd1c7a9d242794ae02e7b76c338a
userman95 <orestismail71@gmail.com>
2019-05-27 19:29:21 +0300

    make chenges for heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_112152) do
+ActiveRecord::Schema.define(version: 2019_05_27_162647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2019_05_27_112152) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
@@ -62,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_05_27_112152) do
     t.string "provider"
     t.string "uid"
     t.text "image"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
