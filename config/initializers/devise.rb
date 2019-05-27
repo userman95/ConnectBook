0479e4662d84d60fccef6dd214cd3c5e863c1a14
userman95 <orestismail71@gmail.com>
2019-05-27 19:13:28 +0300

    make chenges for heroku deploy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,7 +260,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook,'887583771575581', "a714ac900c8174ada1d1da51e34216a0", token_params: { parse: :json }
+  config.omniauth :facebook,'887583771575581', "a714ac900c8174ada1d1da51e34216a0", callback_url:  "https://safe-falls-41970.herokuapp.com/users/auth/facebook/callback"
+
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
