12b69d4c27159cbff3cd2f4171fa601f004997fc
userman95 <orestismail71@gmail.com>
2019-05-27 19:40:50 +0300

    make chenges for heroku deploy

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :trackable, :rememberable, :validatable,
+         :recoverable, :rememberable, :trackable , :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :requests,
