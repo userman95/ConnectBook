cd90715d1e72ee3fddc6a8dfb0996bd69dd62d43
userman95 <orestismail71@gmail.com>
2019-05-28 19:28:14 +0300

    Send friend request to user 	-send_request action 	-routes for send_request 	-partial for index action Begin basic html and css lookup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def send_request
+    p params
+    @user = User.find_by(id: params[:id])
+    current_user.requests_to << @user
+    redirect_to users_index_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'users/index'
+  post 'users/send_request' => 'users#send_request'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "users#index"
 
