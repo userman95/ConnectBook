c58ac1a808fd3142b104be551b8b072a209aab40
Efrain Pinto <efrapp@gmail.com>
2019-06-21 08:12:54 -0500

    Fix error on click comment event

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('.comment-button').on("click", function(){
+  $(document).on("click", ".comment-button", function(e){
     var post_id = this.id.split("-")[1];
     $('#post-'+post_id+'-comments').toggle();
   });
