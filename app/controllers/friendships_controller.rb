e6f64b33513a23fff81c85dc1e808d31462848f8
Efrain Pinto <efrapp@gmail.com>
2019-06-21 08:57:02 -0500

    move delete_friend_request action to a callback

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,8 +1,5 @@
 class FriendshipsController < ApplicationController
 
-  before_action :friendship_to_be_destroyed ,only: [:destroy]
-  after_action :destroy_friend_request, only: [:create]
-
   def create
     @friend = User.find_by(id: params[:friend_id])
     @friendship = current_user.active_friendships.build(friend: @friend)
@@ -18,18 +15,10 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
+    @friendship = Friendship.find_by(friend_id: params[:id])
     @friendship.destroy
 
     render format: :js
   end
 
-  private
-
-    def friendship_to_be_destroyed
-      @friendship = Friendship.find_by(friend_id: params[:id])
-    end
-
-    def destroy_friend_request
-      FriendRequest.where(user: @friend, friend: current_user).first.destroy
-    end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,6 +3,7 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   before_validation :check_duplicates, on: :create
+  after_create_commit :delete_friend_request
 
   validates :user, presence: true, uniqueness: { scope: :friend }
   validates :friend, presence: true, uniqueness: { scope: :user }
@@ -31,6 +32,12 @@ class Friendship < ApplicationRecord
     def check_duplicates
       return if Friendship.where(user: user, friend: friend).empty?
       self.user, self.friend = friend, user
+    end
+
+    def delete_friend_request
+      if user && friend
+        FriendRequest.where(user: friend, friend: user).first.destroy
+      end
     end
 
 end
